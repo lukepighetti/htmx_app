7f17b9a66a0d90b4f29a7693245461a82404e4a5
Luke Pighetti <lukepighetti@gmail.com>
2024-03-17 12:40:51 -0400

    setup shelf_router

--- bin/htmx_app.dart
+++ bin/htmx_app.dart
@@ -1,5 +1,20 @@
 import 'package:htmx_app/htmx_app.dart' as htmx_app;
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf_io.dart' as io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${htmx_app.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  var app = Router();
+
+  app.get('/hello', (Request request) {
+    return Response.ok('hello-world');
+  });
+
+  app.get('/user/<user>', (Request request, String user) {
+    return Response.ok('hello $user');
+  });
+
+  var server = await io.serve(app.call, 'localhost', 8080);
+
+  print('Listening on http://${server.address.host}:${server.port}');
 }
--- test/htmx_app_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:htmx_app/htmx_app.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
