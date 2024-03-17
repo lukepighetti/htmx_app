4abf5404546a03ffecaaff1f1a1af6031fa71eee
Luke Pighetti <lukepighetti@gmail.com>
2024-03-17 12:44:09 -0400

    setup static hosting

--- bin/htmx_app.dart
+++ bin/htmx_app.dart
@@ -2,9 +2,17 @@ import 'package:htmx_app/htmx_app.dart' as htmx_app;
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
 
 Future<void> main(List<String> arguments) async {
   var app = Router();
+
+  var handler = createStaticHandler(
+    'static',
+    defaultDocument: 'index.html',
+  );
+
+  app.get('/', handler);
 
   app.get('/hello', (Request request) {
     return Response.ok('hello-world');
